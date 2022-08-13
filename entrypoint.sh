db4dd1a2537fa67e54f68e2a62f229c9c60fd30d
Roy Hasson <royhasson@roys-mbp-2.lan>
2022-08-12 23:57:19 -0400

    fixed path to config

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,8 @@ set -e
 time=$(date)
 echo ::set-output name=time::$time
 
-cat /config
-
 echo "token=${INPUT_API_KEY}" >> /config
 
-#results=`upsolver -c /config catalogs ls`
-
-results=`upsolver -h`
+results=`upsolver -c /config catalogs ls`
 
 echo "::set-output name=query_results::$results"
