f679dea612361210840bc9bbd9868bc211c30e4f
Roy Hasson <royhasson@roys-mbp-2.lan>
2022-08-12 23:35:29 -0400

    fixed typos again

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/sh -l
 set -e
 
-echo "Hello ${INPUT_PROFILE_NAME} | ${INPUT_API_KEY}"
-
 time=$(date)
 echo ::set-output name=time::$time
 
 echo "token=${INPUT_API_KEY}" >> config
 
-results = upsolver -c config -p ${INPUT_PROFILE_NAME} catalogs ls
+cat config
+
+results=`upsolver -c config -p ${INPUT_PROFILE_NAME} catalogs ls`
 
 echo "::set-output name=query_results::$results"
