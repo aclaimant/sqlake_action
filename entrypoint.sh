baf7ee1727ac8df1855bd0546fa4c546ee11e04f
Roy Hasson <royhasson@roys-mbp-2.lan>
2022-08-15 13:29:29 -0400

    echo results to github step summary

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,6 @@ echo ::set-output name=time::$time
 echo "token=${INPUT_API_KEY}" >> /config
 
 ## execute script with worksheet path
-python3 /executeworksheet.py -w ${INPUT_WORKSHEET_PATH} -o ${GITHUB_WORKSPACE}
+python3 /executeworksheet.py -w ${INPUT_WORKSHEET_PATH} -o ${env.GITHUB_WORKSPACE}
 
 #echo "::set-output name=query_results::$results"
