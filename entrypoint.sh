6fe242a479f675d475309908666cb38f3ab1d198
Roy Hasson <royhasson@roys-mbp-2.lan>
2022-09-13 21:58:22 -0400

    rename worksheet_path to path_to_sql

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,6 @@ echo ::set-output name=time::$time
 echo "token=${INPUT_API_KEY}" >> /config
 
 ## execute script with worksheet path
-python3 /executeworksheet.py -w ${INPUT_WORKSHEET_PATH} -o ${GITHUB_WORKSPACE}
+python3 /executeworksheet.py -w ${INPUT_PATH_TO_SQL} -o ${GITHUB_WORKSPACE}
 
 #echo "::set-output name=query_results::$results"
