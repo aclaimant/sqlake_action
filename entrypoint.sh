cc3faab7a69c83a5383cd985f98036fc0f78e322
Roy Hasson <royhasson@roys-mbp-2.lan>
2022-09-27 13:04:00 -0400

    debuging

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,13 +7,15 @@ echo ::set-output name=time::$time
 ## read API token from input and add it to the CLI configuration file
 echo "token=${INPUT_API_KEY}" >> /config
 
-if [ ! -z "${INPUT_PATH_TO_SQL}" ]
+if [ "${INPUT_PATH_TO_SQL}" ]
 then 
   ## execute script with worksheet path
+  echo "${INPUT_PATH_TO_SQL}"
   python3 /executeworksheet.py -w ${INPUT_PATH_TO_SQL} -o ${GITHUB_WORKSPACE}
-elif [ ! -z "${INPUT_FILE_LIST}" ]
+elif [ "${INPUT_FILE_LIST}" ]
 then
   ## execute script with worksheet path
+  echo "${INPUT_FILE_LIST}"
   python3 /executeworksheet.py -f ${INPUT_FILE_LIST} -o ${GITHUB_WORKSPACE}
 fi
 
